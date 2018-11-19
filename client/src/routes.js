478749e6eda30ce3fee9d05b17c081e5d926d2bb
manganet <manganet55@gmail.com>
2018-11-19 22:07:09 +0400

    register_login

---
+++ client/src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+
+import Layout from './hoc/layout';
+import Home from './components/Home';
+import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/register';
+
+import UserDashboard from './components/User';
+
+const Routes = () => {
+  return ( 
+    <Layout>
+      <Switch>
+        <Route path = "/user/dashboard" exact component = {UserDashboard}/>
+        
+        <Route path = "/register" exact component = {Register}/>
+        <Route path = "/register_login" exact component = {RegisterLogin}/>
+        <Route path = "/" exact component = {Home}/>
+      </Switch>
+    </Layout>
+
+  )
+}
+
+
+export default Routes;
--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  next();
+});
+
 // Models
 const {
   User
