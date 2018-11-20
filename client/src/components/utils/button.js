74dd7c4f50f2f48aa451cf998f3363dbbc17e0e9
manganet <manganet55@gmail.com>
2018-11-20 17:59:51 +0400

    server shop route

--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
+
 const MyButton = (props) => {
   
   const buttons = () => {
@@ -9,12 +12,24 @@ const MyButton = (props) => {
     switch(props.type){
       case "default":
         template = <Link
-          className="link_default"
-        to={props.linkTo}
-        {...props.addStyles}
+          className={!props.altClass ? 'link_default': props.altClass}
+          to={props.linkTo}
+          {...props.addStyles}
         >
           {props.title}
           </Link>
+      break;
+      case "bag_link":
+        template =
+          <div className="bag_link"
+          onClick={() => {
+            props.runAction();
+            }}
+          >
+          <FontAwesomeIcon
+            icon={faShoppingBag}
+          />
+          </div>
       break;
       default:
         template='';
