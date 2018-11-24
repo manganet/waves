ad89465f9fae391382eee85a85fda37ffcf50618
manganet <manganet55@gmail.com>
2018-11-24 12:38:30 +0400

    change package.json bcrytjs

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const SALT_I = 10;
 require('dotenv').config();
@@ -47,9 +47,9 @@ userSchema.pre('save', function (next) {
   var user = this;
 
   if (user.isModified('password')) {
-    bcrypt.genSalt(SALT_I, function (err, salt) {
+    bcryptjs.genSalt(SALT_I, function (err, salt) {
       if (err) return next(err);
-      bcrypt.hash(user.password, salt, function (err, hash) {
+      bcryptjs.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
         user.password = hash;
         next();
@@ -61,7 +61,7 @@ userSchema.pre('save', function (next) {
 })
 
 userSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+  bcryptjs.compare(candidatePassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch)
   })
